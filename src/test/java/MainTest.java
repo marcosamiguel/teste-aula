8d84489f143dab1ed640fa73abbc56556f6a6644
marcosmiguel <marcos.a.miguel@gmail.com>
2024-02-21 15:26:24 -0300

    teste inicial

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.example.Main;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+
+    @Test
+    public void testeNumero(){
+
+        assertEquals(Main.teste(),"1;2");
+
+
+    }
+}
